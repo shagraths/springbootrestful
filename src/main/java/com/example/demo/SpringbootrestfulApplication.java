7d2244d7045785592730894dcee822d210152176
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-04-06 12:24:58 -0400

    crear proyecto spring web

---
+++ src/main/java/com/example/demo/SpringbootrestfulApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestfulApplication.class, args);
+	}
+}
