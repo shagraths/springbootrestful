47b1e50a121f8a7938761edea2f4754fe22900b2
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-04-13 08:38:20 -0400

    custom exception

---
+++ src/main/java/com/example/demo/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Empleado;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long>{
+
+}
