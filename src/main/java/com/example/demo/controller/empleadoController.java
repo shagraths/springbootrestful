47ffbc92dc52e1cfab79f59086d52d9a5e4ec2e3
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-04-13 11:19:49 -0400

    traer todos los empleados y por id

--- src/main/java/com/example/demo/controller/empleadoController.java
+++ src/main/java/com/example/demo/controller/empleadoController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +30,16 @@ public class empleadoController {
 		return new ResponseEntity<Empleado>(empleadoService.guardarEmpleado(empleado), HttpStatus.CREATED);
 	}
 
+	// constriur traer todos los empleados rest api
+	@GetMapping
+	public List<Empleado> traerEmpleados() {
+		return empleadoService.traerEmpleados();
+	}
+
+	// construir obtener empleado por id rest api
+	// http://localhost:8080/api/empleado/1
+	@GetMapping("{id}")
+	public ResponseEntity<Empleado> traerEmpleadoPorId(@PathVariable("id") long empleadoId) {
+		return new ResponseEntity<Empleado>(empleadoService.traerEmpleadoPorId(empleadoId), HttpStatus.OK);
+	}
 }
