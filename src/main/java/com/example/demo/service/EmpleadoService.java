d298fbda987d5cdfb5bc3bf223998a8aa7d41ab3
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-04-13 12:10:23 -0400

    método eliminar empleado

--- src/main/java/com/example/demo/service/EmpleadoService.java
+++ src/main/java/com/example/demo/service/EmpleadoService.java
@@ -6,7 +6,12 @@ import com.example.demo.model.Empleado;
 
 public interface EmpleadoService {
 	Empleado guardarEmpleado(Empleado empleado);
+
 	List<Empleado> traerEmpleados();
+
 	Empleado traerEmpleadoPorId(long id);
+
 	Empleado actualizarEmpleadoPorId(Empleado empleado, long id);
+
+	void eliminarEmpleado(long id);
 }
